0193e96d6bef8f4a2bd88bd47ccec5a5f27bd219
MaoBA1 <94516860+MaoBA1@users.noreply.github.com>
2022-04-11 09:43:34 +0300

    123

---
+++ src/screens/cart/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+const CartScreen = props => {
+    return(
+        <View>
+            <Text>Cart Screen</Text>
+        </View>
+    )
+}
+
+
+export default CartScreen;
